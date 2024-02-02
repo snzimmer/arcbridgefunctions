4daf2f629836d71f4e5497a8d6c58050566d3d2c
snzimmer <62906192+snzimmer@users.noreply.github.com>
2024-02-02 09:08:52 -0700

    Add function to check contents of a .gdb

---
+++ R/check_gdb_contents.R
@@ -0,0 +1,13 @@
+#' Check the contents of a file geodatabase (.gdb) so features and rasters contained in it can be opened.
+#'
+#' This function simply takes a  path to a file geodatabase, and shows the contents of the it.
+#'
+#'
+#' @param path Full file path to an existing geodatabase (.gdb)
+#'
+#'
+#' @export
+#'
+check_gdb_contents<- function(file_path,...){
+  arc.open(file_path)@children
+}
