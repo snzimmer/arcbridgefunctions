22e196fbc727e099049c8192b7e8af71526300a2
snzimmer <62906192+snzimmer@users.noreply.github.com>
2024-02-02 09:19:47 -0700

    Update check_gdb_contents.R

--- R/check_gdb_contents.R
+++ R/check_gdb_contents.R
@@ -1,6 +1,6 @@
-#' Check the contents of a file geodatabase (.gdb) so features and rasters contained in it can be opened.
+#' Check the contents of a file geodatabase (.gdb)
 #'
-#' This function simply takes a  path to a file geodatabase, and shows the contents of the it.
+#' This function simply takes a path to a file geodatabase (.gdb), and shows the contents of it so features and rasters contained in it can be opened.
 #'
 #'
 #' @param file_path Full file path to an existing geodatabase (.gdb)
